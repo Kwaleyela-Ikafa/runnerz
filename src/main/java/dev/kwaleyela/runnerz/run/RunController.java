a517d8f196928c34cdfeab3a9e6bb43c50706758
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-07-02 11:18:56 +0200

    create run controller

---
+++ src/main/java/dev/kwaleyela/runnerz/run/RunController.java
@@ -0,0 +1,64 @@
+package dev.kwaleyela.runnerz.run;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
+@RestController
+@RequestMapping("/api/runs")
+public class RunController {
+    private final RunRepository runRepository;
+
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    @GetMapping("")
+    public List<Run> findAll() {
+        return runRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Run findById(@PathVariable Integer id) {
+        Optional<Run> run = runRepository.findById(id);
+        if(run.isEmpty()) {
+            throw new  RunNotFoundException(id);
+        }
+        return run.get();
+    }
+    
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@Valid @RequestBody Run run) {
+        runRepository.createRun(run);
+    }
+    
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
+        runRepository.updateRun(run, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
+    }
+}
