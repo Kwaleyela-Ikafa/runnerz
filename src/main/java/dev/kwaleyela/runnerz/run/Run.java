de556f6f88051d7cdc807497fab76fe5cf3e9aec
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-07-02 11:18:42 +0200

    add validation constraints to run record

--- src/main/java/dev/kwaleyela/runnerz/run/Run.java
+++ src/main/java/dev/kwaleyela/runnerz/run/Run.java
@@ -2,11 +2,21 @@ package dev.kwaleyela.runnerz.run;
 
 import java.time.LocalDateTime;
 
+import jakarta.annotation.Nonnull;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Positive;
+
 public record Run(
     Integer id,
+    @NotEmpty
     String title,
+    @Past
     LocalDateTime startedOn,
+    @Future
     LocalDateTime completedOn,
+    @Positive
     Integer miles,
     Location location
 ) {}
